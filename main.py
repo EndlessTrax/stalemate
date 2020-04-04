fb33e4a6a53fa61e96024fc5526bfa8bc9b73ee9
endlesstrax <ricky@whitelionmedia.com>
2020-04-04 18:51:34 -0400

    working archives list returned

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import stalemate
+from stalemate import archives
 
 
 if __name__ == "__main__":
     username = "endlesstrax"
 
-    
+    url = archives.build_archive_endpoint(username)
+    user_archives = archives.fetch_archives_list(url)
+
+    print(user_archives)
