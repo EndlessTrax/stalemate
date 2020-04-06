807700080351a82afcf12d54687aaeb8c0679556
endlesstrax <ricky@whitelionmedia.com>
2020-04-05 22:06:28 -0400

    Added ability to get sorted list of all ECOS

--- stalemate/game_parser.py
+++ stalemate/game_parser.py
@@ -2,7 +2,10 @@
 TODO:
 """
 
+import os
 import re
+import json
+
 
 
 def get_game_eco(game):
@@ -22,3 +25,25 @@ def get_game_eco(game):
         eco = "None"
 
     return eco
+
+
+def eco_played_totals(files_dir):
+    played_ecos = dict()
+    list_of_files = os.listdir(files_dir)
+
+    for game_file in list_of_files:
+        with open(f"{files_dir}/{game_file}", 'r', encoding='utf-8') as f:
+            games_in_file = json.load(f)
+
+            for game in games_in_file:
+                game_eco = get_game_eco(game)
+                
+                if game_eco in played_ecos:
+                    val = played_ecos.get(game_eco)
+                    played_ecos[game_eco] = val + 1
+                else:
+                    played_ecos[game_eco] = 1
+
+    sorted_ecos = sorted(played_ecos.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_ecos
