3e7bd9cab691424c0744353330d35c2ea3bc77c8
endlesstrax <ricky@whitelionmedia.com>
2020-04-06 10:10:16 -0400

    Futher tests added for functions in game_parser.py

--- tests/test_game_parser.py
+++ tests/test_game_parser.py
@@ -1,13 +1,34 @@
 import pytest
 import json
-from stalemate.game_parser import game_result
+from stalemate.game_parser import (
+    get_game_eco, eco_played_totals, game_result, eco_win_lose_draw_totals
+    )
+
+
+user = 'endlesstrax'
 
 with open('game_data/2008-07.json', 'r', encoding='utf-8') as game_file:
     games = json.load(game_file)
 
 
-user = 'endlesstrax'
+def test_get_game_eco():
+    game = games[0]
+    eco = get_game_eco(game)
+    assert eco == "C23"
+
+
+def test_eco_played_totals():
+    sorted_list = eco_played_totals('game_data')
+    assert isinstance(sorted_list, list)
+    assert sorted_list[0] != None
+
 
 def test_game_result():
     result = game_result(game=games[0], user=user)
     assert result == "checkmated"
+
+
+def test_eco_win_lose_draw_totals():
+    eco_dict = eco_win_lose_draw_totals("A00", "game_data")
+    assert isinstance(eco_dict, dict)
+    assert eco_dict.get("ECO") == "A00"
