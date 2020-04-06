1fab3471f2519d1e8479713ee7f27c1662f3456c
endlesstrax <ricky@whitelionmedia.com>
2020-04-06 10:10:01 -0400

    Futher tests added for functions in archives.py

--- tests/test_archives.py
+++ tests/test_archives.py
@@ -1,10 +1,25 @@
 import pytest
 import requests
 from stalemate.endpoints import archives_url
+from stalemate.archives import build_archive_endpoint, fetch_archives_list
 
+
+test_username = "endlesstrax"
 
 def test_is_valid_url():
-    username = "endlesstrax"
-    r = requests.get(archives_url.format(username))
+    r = requests.get(archives_url.format(test_username))
     assert r.status_code == 200
+
+
+def test_build_archive_endpoint():
+    url = build_archive_endpoint(test_username)
+    assert url == f"https://api.chess.com/pub/player/{test_username}/games/archives"
+    assert url == "https://api.chess.com/pub/player/endlesstrax/games/archives"
+
+
+def test_fetch_archives_list():
+    url = build_archive_endpoint(test_username)
+    archives = fetch_archives_list(url)
+    assert isinstance(archives, list)
+
 
